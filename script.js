505bfd24a37f36d676f1695f7f534f614afecb20
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-07-22 09:07:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(e) {
+	e.preventDefault();
+	const inputs = document.getElementsByTagName("input");
+	const val1 = inputs[0].value;
+	const val2 = inputs[1].value;
+	alert(`${val1} ${val2}`)
 
 }
